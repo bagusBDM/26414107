e758e4ec3017ad92c07232f43b7f5f6418e36d3a
PaskahAbr96 <paskah.abraham96@gmail.com>
2016-10-24 11:14:14 +0700

    24 Okt 2016

---
+++ asdf.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# Function definition
+sub Average{
+   # get total number of arguments passed.
+   $n = scalar(@_);
+   $sum = 0;
+   foreach $item (@_){
+      $sum += $item;
+   }
+   $average = $sum / $n;
+   print "Rata-rata : $average\n";
+}
+# Function call
+Average(2, 4, 6);
